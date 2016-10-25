5251706d9d3823fb7c2154c38fce2ca8f888149a
T-Bone Lu <infinityonhigh9669@gmail.com>
2016-10-25 22:55:44 +0800

    no message

--- ex1-3.sh
+++ ex1-3.sh
@@ -3,3 +3,30 @@
 
 echo "$# parameters"
 echo "$@"
+
+
+#輸出參數索引
+echo "OPTIND starts at $OPTIND"
+#接收參數
+while getopts ":pq:" optname 
+	do
+	case "$optname" in
+		"p")
+			echo "Option #optname is specified"
+			;;
+		"q")
+			echo "Option #optname has value $OPTARG"
+			;;
+		"?")
+			echo "Unknow Option $OPTARG"
+			;;
+		":")
+			echo "No argument value for Option $OPTARG"
+			;;
+		*)
+			#shoud not occured
+			echo "Unknow Option error while proccessing options"
+			;;
+	esac
+	echo "Option is now $OPTIND"
+done
